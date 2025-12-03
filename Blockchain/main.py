b0d2db148fdad9fd4aababd89096663ddc309402
Emerson <edavid.manaog@gmail.com>
2025-12-03 11:32:41 +1100

    update blockchain

--- Blockchain/main.py
+++ Blockchain/main.py
@@ -1,60 +1,78 @@
-import hashlib
-import time
-class Blockchain:
-    def __init__(self):
-        self.chain = [self.create_genesis_block()]
+from backend.blockchain.block import Block
 
-    def create_genesis_block(self):
-        timestamp = time.time()
-        return Block(0, "0", timestamp, "Genesis Block", calculate_hash(0, "0", timestamp, "Genesis Block"))
+class Blockchain:
+    """
+    Blockchain: a public ledger of transactions.
+    Implemented as a list of blocks - data sets of transactions
+    """
+    def __init__(self):
+        self.chain = [Block.genesis()]
 
     def add_block(self, data):
-        previous_block = self.chain[-1]
-        new_block = create_new_block(previous_block, data)
-        self.chain.append(new_block)
+        self.chain.append(Block.mine_block(self.chain[-1], data))
 
-    def print_chain(self):
-        print_blockchain(self.chain)
-class Block:
-    def __init__(self, index, previous_hash, timestamp, data, hash):
-        self.index = index
-        self.previous_hash = previous_hash
-        self.timestamp = timestamp
-        self.data = data
-        self.hash = hash
+    def __repr__(self):
+        return f'Blockchain: {self.chain}'
 
-def calculate_hash(index, previous_hash, timestamp, data):
-    value = f"{index}{previous_hash}{timestamp}{data}".encode()
-    return hashlib.sha256(value).hexdigest()
+    def replace_chain(self, chain):
+        """
+        Replace the local chain with the incoming one if the following applies:
+          - The incoming chain is longer than the local one.
+          - The incoming chain is formatted properly.
+        """
+        if len(chain) <= len(self.chain):
+            raise Exception('Cannot replace. The incoming chain must be longer.')
 
-def create_genesis_block():
-    timestamp = time.time()
-    return Block(0, "0", timestamp, "Genesis Block", calculate_hash(0, "0", timestamp, "Genesis Block"))
+        try:
+            Blockchain.is_valid_chain(chain)
+        except Exception as e:
+            raise Exception(f'Cannot replace. The incoming chain is invalid: {e}')
 
-def create_new_block(previous_block, data):
-    index = previous_block.index + 1
-    timestamp = time.time()
-    hash = calculate_hash(index, previous_block.hash, timestamp, data)
-    return Block(index, previous_block.hash, timestamp, data, hash)
+        self.chain = chain
 
-def print_blockchain(blockchain):
-    for block in blockchain:
-        print(f"Block {block.index}:")
-        print(f"  Previous Hash: {block.previous_hash}")
-        print(f"  Data: {block.data}")
-        print(f"  Hash: {block.hash}")
-        print(f"  Timestamp: {block.timestamp}")
-        print("-" * 40)
+    def to_json(self):
+        """
+        Serialize the blockchain into a list of blocks.
+        """
+        return list(map(lambda block: block.to_json(), self.chain))
 
-# Create the blockchain
-blockchain = [create_genesis_block()]
-previous_block = blockchain[0]
+    @staticmethod
+    def from_json(chain_json):
+        """
+        Deserialize a list of serialized blocks into a Blokchain instance.
+        The result will contain a chain list of Block instances.
+        """
+        blockchain = Blockchain()
+        blockchain.chain = list(
+            map(lambda block_json: Block.from_json(block_json), chain_json)
+        )
 
-# Add 6 more blocks
-for i in range(1, 7):
-    new_block = create_new_block(previous_block, f"Block {i} Data")
-    blockchain.append(new_block)
-    previous_block = new_block
+        return blockchain
 
-# Print the blockchain
-print_blockchain(blockchain)
+    @staticmethod
+    def is_valid_chain(chain):
+        """
+        Validate the incoming chain.
+        Enforce the following rules of the blockchain:
+          - the chain must start with the genesis block
+          - blocks must be formatted correctly
+        """
+        if chain[0] != Block.genesis():
+            raise Exception('The genesis block must be valid')
+
+        for i in range(1, len(chain)):
+            block = chain[i]
+            last_block = chain[i-1]
+            Block.is_valid_block(last_block, block)
+
+
+def main():
+    blockchain = Blockchain()
+    blockchain.add_block('one')
+    blockchain.add_block('two')
+
+    print(blockchain)
+    print(f'blockchain.py ___name__: {__name__}')
+
+if __name__ == '__main__':
+    main()
