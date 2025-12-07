8c6b104c5462d7a2320152d8243f593f5707b480
Emerson <edavid.manaog@gmail.com>
2025-12-08 10:14:35 +1100

    Added Block. function and import

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -1,16 +1,14 @@
-from block import Block, genesis, mine_block
+from block import Block
 class Blockchain:
     """
     Blockchain: a public ledger of transactions.
     Implemented as a list of blocks - data sets of transactions
     """
     def __init__(self):
-        self.chain = [genesis()]
+        self.chain = [Block.genesis()]
 
     def add_block(self, data):
-        last_block = self.chain[-1]
-
-        self.chain.append(mine_block(last_block, data))
+        self.chain.append(Block.mine_block(self.chain[-1], data))
 
     def __repr__(self):
         return f'Blockchain: {self.chain}'
